64fb506a8e683bb821975a1901dc60750683fc75
Opilane TTHK <opilane@tthk.ee>
2022-02-28 10:12:53 +0200

    www

---
+++ Cats and Dogs/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Cats_and_Dogs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listpets = new System.Windows.Forms.ListBox();
+            this.Pets = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listpets
+            // 
+            this.listpets.FormattingEnabled = true;
+            this.listpets.Location = new System.Drawing.Point(66, 178);
+            this.listpets.Name = "listpets";
+            this.listpets.Size = new System.Drawing.Size(120, 95);
+            this.listpets.TabIndex = 0;
+            this.listpets.SelectedIndexChanged += new System.EventHandler(this.listpets_SelectedIndexChanged);
+            // 
+            // Pets
+            // 
+            this.Pets.AutoSize = true;
+            this.Pets.Location = new System.Drawing.Point(66, 60);
+            this.Pets.Name = "Pets";
+            this.Pets.Size = new System.Drawing.Size(28, 13);
+            this.Pets.TabIndex = 1;
+            this.Pets.Text = "Pets";
+            this.Pets.Click += new System.EventHandler(this.Pets_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Pets);
+            this.Controls.Add(this.listpets);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listpets;
+        private System.Windows.Forms.Label Pets;
+    }
+}
+
---
+++ Cats and Dogs/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cats_and_Dogs
+{
+    public partial class Form1 : Form
+    {
+        string connectionStrings;
+        SqlConnection connection;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Pets_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listpets_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
